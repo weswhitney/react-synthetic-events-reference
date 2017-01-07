66d781018fd4abc9fe54e4b24b51a39c10d32209
weswhitney <wesleywhitney@gmail.com>
2017-01-06 23:44:00 -0700

    textarea examples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Textfield from './components/Textfield.js';
+import Textarea from './components/Textarea.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
       <Textfield />
+      <Textarea />
+    </div>
     );
   }
 }
---
+++ src/components/Textarea.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+const Textarea = React.createClass({
+      getInitialState: function () {
+          return {value: "I'm changeable and under React's control" };
+      },
+
+      handleChange: function (e) {
+        const nextValue = e.target.value;
+
+        this.setState({value: nextValue});
+      },
+
+      render: function () {
+        return (
+          <div className={"container"}>
+              <form className={"bs-example bs-example-form"}>
+                <div className={"input-group-large"}>
+                    <textarea
+                    className={"form-control"}
+                    onChange={this.handleChange}
+                    value={this.state.value}
+                  />
+                </div>
+                  <div className={"input-group-large"}>
+                  <textarea
+                    className={"form-control"}
+                      value={"I'm read-only, am under React's control, but raise a warning."}
+                  />
+                </div>
+                <div className={"input-group-large"}>
+                  <textarea
+                    className={"form-control"}
+                    defaultValue={"I'm changeable but am not under React's control."}
+                  />
+                </div>
+                <div className={"input-group-large"}>
+                  <textarea
+                    className={"form-control"}
+                    readOnly={true}
+                    value={"I'm read-only and under React's control."}
+                  />
+                </div>
+              </form>
+          </div>
+        );
+      }
+  });
+
+export default Textarea;

73b5a54e08f550a1587286a5f503e27eecb519aa
Wesley Whitney <wesleywhitney@gmail.com>
2017-01-10 08:15:30 -0700

    checkbox's component

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -10,29 +10,49 @@ const Checkbox = React.createClass({
   render: function () {
     return (
       <div>
-      <h1>checkbox</h1>
+
+        <h1>checkbox</h1>
+
         <div className={"input-group"}>
           <span className={"input-group-addon"}>
-            <input checked={this.state.checked} type={"checkbox"} onChange={this.handleChange}/>
+            <input
+              checked={this.state.checked}
+              type="checkbox"
+              onChange={this.handleChange}
+            />
           </span>
           {"I'm changeable and under React's control."}
         </div>
 
         <div className={"input-group"}>
           <span className={"input-group-addon"}>
-            <input type={"checkbox"} checked={true} />
+            <input
+              type="checkbox"
+              checked={true}
+            />
           </span>
           {"I'm read only, am under React's control, but raise a warning."}
         </div>
 
         <div className={"input-group"}>
           <span className={"input-group-addon"}>
-          <input
-            defaultChecked={true}
-            type="checkbox"
-          />
+            <input
+              defaultChecked={true}
+              type="checkbox"
+            />
           </span>
           {"I'm changeable but am not under React's control."}
+        </div>
+
+        <div className={"input-group"}>
+          <span className={"input-group-addon"}>
+            <input
+              checked={true}
+              readOnly={true}
+              type="checkbox"
+            />
+          </span>
+          {"I'm read-only and under React's control."}
         </div>
       </div>
     );

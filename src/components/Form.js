faa0d1872f3c9664c34f4a2111a1deb04619e45d
weswhitney <wesleywhitney@gmail.com>
2017-01-12 21:31:44 -0700

    form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 
 const Form = React.createClass({
+  getInitialState: function () {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  handleChange: function () {
+    const nextState = { [event.target.name]: event.target.value };
+
+    this.setState(nextState);
+  },
   render: function () {
     return (
-      <div>form</div>
+      <div>
+        <h1>form</h1>
+        <form>
+          <label>
+            Username:
+            <input
+              name="username"
+              onChange={this.handleChange}
+              type="text"
+              value={this.state.username}
+            />
+          </label>
+
+          <label>
+            Password:
+            <input
+              name="password"
+              onChange={this.handleChange}
+              type="password"
+              value={this.state.password}
+            />
+          </label>
+          <input type="submit" value="Log in"/>
+        </form>
+      </div>
     );
   }
 });

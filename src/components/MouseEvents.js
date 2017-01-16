9295ff8609258df2855d0662e09b74085db2842d
weswhitney <wesleywhitney@gmail.com>
2017-01-16 10:52:12 -0700

    mouse events component

--- src/components/MouseEvents.js
+++ src/components/MouseEvents.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 
 const MouseEvents = React.createClass({
+  getInitialState: function () {
+    return {
+      clicks: 0,
+      location: null,
+      pageX: null,
+      pageY: null
+    };
+  },
+  handleClick: function () {
+    const nextClicks = this.state.clicks + 1;
+
+    this.setState({clicks: nextClicks})
+  },
+  handleMouseEnter: function () {
+    this.setState({location: 'Entered'});
+  },
+  handleMouseLeave: function () {
+    this.setState({location: 'Left'});
+  },
+  handleMouseMove: function (event) {
+    const nextPageX = event.pageX;
+    const nextPageY = event.pageY;
+
+    this.setState({pageX: nextPageX, pageY: nextPageY});
+  },
   render: function () {
     return (
-      <div>mouse events</div>
+      <div>
+
+        <h1>mouse events</h1>
+
+        <div
+          onClick={this.handleClick}
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+          style={{backgroundColor: 'lightblue', height: '300px', width: '300px'}}
+        />
+        <div>clicks: {this.state.clicks}</div>
+        <div>location: {this.state.location}</div>
+        <div>pageX: {this.state.pageX}</div>
+        <div>pageY: {this.state.pageY}</div>
+      </div>
     );
   }
 });
